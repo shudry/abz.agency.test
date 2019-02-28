280b7e3afb558158d3496aedf5940a6c7f45693a
shudry <shumilo.dmytry@gmail.com>
2019-02-28 21:37:40 +0200

    Add authorization view

--- manager_employee/urls.py
+++ manager_employee/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MainPage
+from .views import MainPage, AuthenticateUser
 
 urlpatterns = [
-	path('', MainPage.as_view(), name='main_page'),
+    path('login/', AuthenticateUser.as_view()),
+    path('', MainPage.as_view(), name='main_page'),
 ]
--- manager_employee/views.py
+++ manager_employee/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseNotFound
 
 from .models import Employee
 # Create your views here.
@@ -23,3 +24,28 @@ class MainPage(View):
     def get_static_context(self):
         self.context_data['page_name'] = "Main context test"
         self.context_data['workers'] = Employee.objects.all()
+
+
+class AuthenticateUser(View):
+
+    def get(self, request):
+        return HttpResponseNotFound('<h3>Not found this login page</h3>')
+
+    def post(self, request):
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if username is None or password is None:
+            return HttpResponseNotFound('<h1>Argument error</h1>')
+
+        if not User.objects.filter(username=username):
+            return HttpResponseNotFound('<h1>User not found</h1>')
+
+        user_auth = authenticate(username=user.username, password=password)
+
+        if user_auth is not None:
+            if user_auth.is_active:
+                auth_login(request, user_auth)
+                return redirect('/')
+
+        return HttpResponseNotFound('<h1>Password is not valid!</h1>')
